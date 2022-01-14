bddc48d735b613d56b0d5d6129d86b595ac93d2c
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-14 21:50:05 +0100

    Initial commit

---
+++ 01_cr/host/main.cpp
@@ -0,0 +1,18 @@
+#define CR_HOST
+#define CR_WINDOWS
+#include "cr.h"
+
+int main(int argc, char* argv[])
+{
+    cr_plugin ctx;
+    cr_plugin_open(ctx, "C:/Dev/HelloCppHotReloading/01_cr/module.dll");
+
+    while (true)
+    {
+        cr_plugin_update(ctx);
+    }    
+
+    cr_plugin_close(ctx);
+
+    return 0;
+}
